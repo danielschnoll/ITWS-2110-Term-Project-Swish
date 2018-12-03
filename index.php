489c718e58a03bfd9cc26abab061eb8508c1986c
Daniel <schnolldj@gmail.com>
2018-12-03 01:03:30 -0500

    Fixed some naming issues

--- index.php
+++ index.php
@@ -53,8 +53,8 @@
     // This block is for the links on the right.
     echo '<ul class="right hide-on-med-and-down">';
     echo '<li class="active"><a id="navlinks" href="index.php">Home</a></li>';
-    echo '<li><a id="navlinks" href="teams.php">Teams</a></li>';
-    echo '<li><a id="navlinks" href="roaster.php">Roaster</a></li>';
+    echo '<li><a id="navlinks" href="teams.php">Team Management</a></li>';
+    echo '<li><a id="navlinks" href="roster.php">Roster</a></li>';
     
     echo '<li><a id="navlinks" href="login.php">Logout</a></li>';
     echo '<li><a id="navlinks" href="logout_action.php">Logout</a></li>';
