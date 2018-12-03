d4848d3373b0107a4d0c2fd9683b43748a5028c4
Jenny <jennymlee351@gmail.com>
2018-12-03 01:33:51 -0500

    roaster update

--- includes/eachTeam.php
+++ includes/eachTeam.php
@@ -3,7 +3,7 @@
 <div class="row">
   <div class="col s12 m12">
   	<?php $color = "card ". $colors[rand(0,5)] ." darken-1" ?>
-    <div class="<?php  echo($color);?>">
+    <div class="<?php  echo($color);?> ">
       <div class="card-content white-text">
         	
         <table>
