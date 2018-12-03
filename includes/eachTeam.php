7e33f9f6ed88a64eb945b2db8a0b33c13fe73eb4
Mark Robinson <robinm8@rpi.edu>
2018-12-03 13:48:17 -0500

    Fix color code

--- includes/eachTeam.php
+++ includes/eachTeam.php
@@ -2,24 +2,14 @@
 
 <div class="row">
   <div class="col s12 m12">
-
-    <?php 
-    $randomNum = rand(0,9);
-    error_reporting(0);
-    while (in_array($randomNum, $numsUsed) && $numsUsed.length < $numTeamMembers) {
+    <?php
       $randomNum = rand(0,9);
-    
-    }
-
-    array_push($numsUsed, $randomNum);
-    //echo(sizeof($numsUsed));
-    //echo $randomNum;
-    
-    $color = "card ". $colors[$randomNum] ." darken-1";
-     ?>
+      array_push($numsUsed, $randomNum);
+      $color = "card ". $colors[$randomNum] ." darken-1";
+    ?>
     <div class="<?php  echo($color);?>">
       <div class="card-content white-text">
-          
+
         <table>
           <thead>
             <tr>
