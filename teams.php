e68f11288879d3b1f0754d19f0802d2d5b24167e
Jenny <jennymlee351@gmail.com>
2018-12-03 00:44:46 -0500

    Updated nav bar
    Fixed file spelling error as well

--- teams.php
+++ teams.php
@@ -45,6 +45,7 @@
     echo '<ul class="right hide-on-med-and-down">';
     echo '<li><a id="navlinks" href="index.php">Home</a></li>';
     echo '<li class="active"><a id="navlinks" href="teams.php">Teams</a></li>';
+    echo '<li class="active"><a id="navlinks" href="roaster.php">Roster</a></li>';
     echo '<li><a id="navlinks" href="login.php">Logout</a></li>';
     echo '</ul>';
 
